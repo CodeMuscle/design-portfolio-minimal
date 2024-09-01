049c9b4c6f70c85a2e053eae1a51a1854b4042fd
CodeMuscle <investportal99@gmail.com>
2024-09-01 11:02:22 +0530

    Installed dependecies and added static data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,48 @@ import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
+import { DATA } from "@/data/resume";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
+
+export const metadata: Metadata = {
+  metadataBase: new URL(DATA.url),
+  title: {
+    default: DATA.name,
+    template: `%s | ${DATA.name}`
+  },
+  description: DATA.description,
+  openGraph: {
+    title: `${DATA.name}`,
+    description: DATA.description,
+    url: DATA.url,
+    siteName: `${DATA.name}`,
+    locale: "en-US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    }
+  },
+  twitter: {
+    title: `${DATA.name}`,
+    card: 'summary_large_image',
+  },
+  verification: {
+    google: "",
+    yandex: "",
+  },
+};
 
 export default function RootLayout({
   children,
