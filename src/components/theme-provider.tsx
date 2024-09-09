3c58e16e38a016369a5460707466e43e54b04794
CodeMuscle <investportal99@gmail.com>
2024-09-09 10:25:17 +0530

    Components for the main page

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -3,7 +3,6 @@
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps){
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
-
