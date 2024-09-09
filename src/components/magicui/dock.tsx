fb2b7a8ee10fbf747f1514c0190a819f1f88218d
CodeMuscle <investportal99@gmail.com>
2024-09-09 10:29:20 +0530

    Project Card, Hackathon Card, UI - Badge

--- src/components/magicui/dock.tsx
+++ src/components/magicui/dock.tsx
@@ -54,7 +54,7 @@ const Dock = React.forwardRef<HTMLDivElement, DockProps>(
         className={cn(dockVariants({ className }), {
           "items-start": direction === "top",
           "items-center": direction === "middle",
-          "items-end": direction === "bottom",
+          // "items-end": direction === "bottom",
         })}
       >
         {renderChildren()}
